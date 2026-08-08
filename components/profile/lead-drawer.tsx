e92cf7f992f11afa60e70defdf945be14dd08add
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2026-08-08 12:36:23 +0200

    fix drawer

--- components/profile/lead-drawer.tsx
+++ components/profile/lead-drawer.tsx
@@ -4,6 +4,8 @@ import { Drawer, DrawerContent, DrawerFooter } from "@/components/ui/drawer";
 
 import { Service } from "@/lib/types";
 import LeadForm from "./lead-form";
+import { cn } from "@/lib/utils";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 type Props = {
   trainerId: string;
@@ -13,20 +15,29 @@ type Props = {
 };
 
 export function LeadDrawer({ trainerId, open, service, onOpenChange }: Props) {
-  if (!service) {
-    return null;
-  }
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
-    <Drawer open={open} onOpenChange={onOpenChange} showSwipeHandle>
-      <DrawerContent>
-        <div className="flex-1 overflow-y-auto px-4">
-          <LeadForm
-            service={service}
-            trainerId={trainerId}
-            onCancel={() => onOpenChange(false)}
-            onSuccess={() => onOpenChange(false)}
-          />
+    <Drawer
+      open={open}
+      onOpenChange={onOpenChange}
+      swipeDirection={isDesktop ? "right" : "down"}
+      showSwipeHandle={!isDesktop}
+    >
+      <DrawerContent
+        className={cn(
+          isDesktop && "[--drawer-content-width:560px] rounded-xl ",
+        )}
+      >
+        <div className="flex-1 overflow-y-auto px-4 scrollbar">
+          {service && (
+            <LeadForm
+              service={service}
+              trainerId={trainerId}
+              onCancel={() => onOpenChange(false)}
+              onSuccess={() => onOpenChange(false)}
+            />
+          )}
         </div>
 
         <DrawerFooter>{/* przyciski */}</DrawerFooter>
